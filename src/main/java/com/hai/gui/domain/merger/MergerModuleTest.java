b46f50e655630c2b5d17b91300bd41b061c00b73
[valkyrie7] <iamotley7@gmail.com>
2017-05-02 19:33:55 +0300

    added 2 classes to evaluate individual modules and to sort the candidate maps with mapsort

--- src/main/java/com/hai/gui/domain/merger/MergerModuleTest.java
+++ src/main/java/com/hai/gui/domain/merger/MergerModuleTest.java
@@ -1,5 +1,11 @@
 package com.hai.gui.domain.merger;
 
+/**
+ * Created by Sena on 2.05.2017.
+ * this is to use individual modules and evaluate their accuracy and put them into a map<module name, score>
+ *
+ */
+
 import com.hai.gui.data.csp.Domain;
 import com.hai.gui.data.puzzle.Clue;
 import com.hai.gui.data.puzzle.Puzzle;
@@ -12,19 +18,39 @@ import java.util.List;
 import java.util.Map;
 
 
-/**
- * Created by mrsfy on 30-Apr-17.
- */
-public class Merger {
+public class MergerModuleTest {
 
     private Puzzle puzzle;
     private RestClient restClient;
-    public Merger(Puzzle puzzle) {
+    private Map<String,Double> scoreList;
+
+    public MergerModuleTest(Puzzle puzzle) {
         this.puzzle = puzzle;
         restClient = new RestClient();
+        scoreList = new HashMap<>();
     }
     public Puzzle getPuzzle(){
         return puzzle;
+    }
+    private double calculateScore(List<Candidate> candidates){
+        double score = 0;
+        for(Candidate candidate: candidates){
+            boolean found = false;
+            for(String answer :puzzle.getAnswers())
+            {
+                if(answer.equals(candidate)){
+                    found = true;
+                    break;
+                }
+            }
+            if(found){
+                score += candidate.getScore();
+            }
+            else{
+                score -= candidate.getScore();
+            }
+        }
+        return score;
     }
 
     public Map<String, Domain> getDomains() {
@@ -39,11 +65,12 @@ public class Merger {
         return domains;
     }
 
-    private void useModules(Clue clue, Map<String, Domain> domains, boolean isAcross) {
+    void useModules(Clue clue, Map<String, Domain> domains, boolean isAcross) {
         for (RestModule module : RestModule.values()) {
             System.out.println(clue.getValue());
             System.out.println(clue.getClueStart() + ", " + clue.getClueEnd());
             List<Candidate> candidates = restClient.useModule(module, clue.getValue(), clue.getAnswerLength(isAcross));
+            scoreList.put(module.toString(),scoreList.get(module.toString())+calculateScore(candidates));
             domains.put((isAcross ? "A" : "D") + clue.getClueNum(), new Domain(candidates));
         }
     }
